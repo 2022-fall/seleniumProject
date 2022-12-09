5dc4c56a284240b0db08d2f1c2be758ca1dade6d
Akmal Husanov <a.khusanov@gmail.com>
2022-12-08 20:53:58 -0500

    completed drag and drop scenarios

--- webelement_class/drag_drop_action.py
+++ webelement_class/drag_drop_action.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import *
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
 
 from utilities import *
 
@@ -16,7 +17,7 @@ chr_options.add_experimental_option("detach", True)
 # chr_options.add_experimental_option('excludeSwitches',["disable-popup-blocking"])
 driver = webdriver.Chrome(options=chr_options)
 print('maximizing the browser window')
-driver.maximize_window()
+# driver.maximize_window()
 # This sets a sticky timeout to implicitly wait for an element to be found, or a command to complete.
 driver.implicitly_wait(20)
 time.sleep(0)
@@ -32,14 +33,25 @@ try:
     # disable_google_ads(driver)
 
     # code for drag and drop is here
-    # verify drop box text before dropping, expected: 'Drop here'
-    # drag and drop the object into the box
-    # verify drop box text after dropping, expected: 'Dropped!'
+    print("# verify drop box text before dropping, expected: 'Drop here'")
+    drag_obj = driver.find_element(By.ID, draggable_id)
+    drop_obj = driver.find_element(By.ID, droppable_id)
+    print(f"text in drop box, before: '{drop_obj.text}'")
+    assert drop_obj.text == 'Drop here', "Drop box text verification, before drop action, failed."
+
+    print("# drag and drop the object into the box")
+    actions = ActionChains(driver)
+    actions.drag_and_drop(drag_obj, drop_obj).perform()
+    # actions.click_and_hold(drag_obj).release(drop_obj).perform()
+    # actions.click_and_hold(drag_obj).pause(2).release(drop_obj).perform()
+    print("# verify drop box text after dropping, expected: 'Dropped!'")
+    print(f"text in drop box, after: '{drop_obj.text}'")
+    assert drop_obj.text == 'Dropped!', "Drop box text verification, after drop action, failed."
 
     time.sleep(5)
     print("Drag and Drop Test Successfully executed.")
 
-except Exception as err:
+except (Exception) as err:
     time.sleep(2)
     print("Python Exception: test failed with following exception.")
     print(err)
