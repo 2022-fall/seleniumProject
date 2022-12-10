07a003ddb6d58bab3cf476f9a41fdfd27c0e244b
Akmal Husanov <a.khusanov@gmail.com>
2022-12-10 13:39:04 -0500

    update fixture scope

--- tests/test_sample_scenarios1.py
+++ tests/test_sample_scenarios1.py
@@ -5,14 +5,18 @@ print('Starting the sample test scenario.')
 
 @pytest.fixture(scope='module')
 def greet():
+    print("\n----------------- SetUp ----------------")
     print('Helloooo Test Master!')
-    return [2,3,4]
+    yield [2,3,4]
+    print("\n------------------ TearDown ------------")
+    print("fixture steps are completed here.")
+
 
 
 @pytest.mark.scen1case1
 @pytest.mark.scen1
 def test_scen1_case1(greet):
-    print('scen1_case1 starting !')
+    print('\nscen1_case1 starting !')
     print(greet)
     greet.append(5)
     print(greet)
@@ -24,7 +28,7 @@ def test_scen1_case1(greet):
 @pytest.mark.smoketest
 @pytest.mark.scen1case2
 def test_scen1_case2(greet):
-    print("scenario 1 case 2 starting ...")
+    print("\nscenario 1 case 2 starting ...")
     print(greet)
     greet.append(6)
     print(greet)
