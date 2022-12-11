2fc8d46c43bdff7b47cc2f2dc10ed42dd7207832
Akmal Husanov <a.khusanov@gmail.com>
2022-12-11 12:31:48 -0500

    update conftest.py with webdriver steps refactoring forms->test_registration.py

--- src/conftest.py
+++ src/conftest.py
@@ -1,12 +1,30 @@
 # This is a shared fixture between modules
+import time
+
 import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope='session')
-def greet():
+def driver():
     print("\n----------------- SetUp ----------------")
-    print('Helloooo Test Master!')
-    yield [2,3,4]
+    print('Initializing a webdriver for the browser...')
+    # created the object for chromedriver that talks to Chrome Browser
+    chr_options = Options()
+    chr_options.add_experimental_option("detach", True)
+    # chr_options.add_experimental_option('excludeSwitches',["disable-popup-blocking"])
+    driver = webdriver.Chrome(options=chr_options)
+    print('maximizing the browser window')
+    driver.maximize_window()
+    # This sets a sticky timeout to implicitly wait for an element to be found, or a command to complete.
+    driver.implicitly_wait(20)
+    time.sleep(0)
+
+    yield driver
     print("\n------------------ TearDown ------------")
+    print("# close all tabs:")
+    driver.quit()
+    print("TEST Completed!!")
     print("fixture steps are completed here.")
 
